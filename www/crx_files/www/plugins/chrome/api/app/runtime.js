4d4e0b46caec4db39cb2d3113c208657939029f6
Shravan Narayan <shravanrn@google.com>
2013-04-26 14:39:15 -0400

    Support installation of crx files
    Conflicts:
    	www/index.html

---
+++ www/crx_files/www/plugins/chrome/api/app/runtime.js
@@ -0,0 +1,7 @@
+cordova.define("chrome.app.runtime", function(require, exports, module) {// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var Event = require('chrome.common.events');
+exports.onLaunched = new Event('onLaunched');
+});
