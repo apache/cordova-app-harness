e80bb1dcb813eb04f0f4177a7ecdd634fdfc84d4
Braden Shepherdson <braden.shepherdson@gmail.com>
2013-06-14 09:55:42 -0400

    Remove redundant and unused duplicate file, and improve Chrome loading
    It now draws on the same Chrome app bootstrap files as exist in the app
    harness itself. This ensures that the Javascript always matches the
    native side.

---
+++ www/cdvah_js/CrxPackageHandler.js
@@ -0,0 +1,69 @@
+(function(){
+    "use strict";
+    /* global myApp */
+    myApp.run(["AppsService", "ResourcesLoader", "ContextMenuInjectScript", function(AppsService, ResourcesLoader, ContextMenuInjectScript){
+
+        var platformId = cordova.require("cordova/platform").id;
+
+        function copyFile(startUrl, targetLocation){
+            /************ Begin Work around for File system bug ************/
+            if(targetLocation.indexOf("file://") === 0) {
+                targetLocation = targetLocation.substring("file://".length);
+            }
+            /************ End Work around for File system bug **************/
+            return ResourcesLoader.xhrGet(startUrl)
+            .then(function(xhr){
+                return ResourcesLoader.ensureDirectoryExists(targetLocation)
+                .then(function(){
+                    return ResourcesLoader.writeFileContents(targetLocation, xhr.responseText);
+                });
+            });
+        }
+
+        AppsService.registerPackageHandler("crx", {
+            extractPackageToDirectory : function (appName, fileName, outputDirectory){
+                console.log('extracting the crx');
+                var dataToAppend = ContextMenuInjectScript.getInjectString(appName);
+                var platformDirectory = outputDirectory + "/" + platformId + "/";
+                var platformWWWDirectory = platformDirectory + "www/";
+                var cordovaFile = platformWWWDirectory + "cordova.js";
+
+                // We need to
+                // 1) Copy over the files required to convert a crx to a normal web app
+                // 2) Modify the cordova.js file
+                return ResourcesLoader.extractZipFile(fileName, platformWWWDirectory)
+                .then(function(){
+                    // Read cordova_plugins.json and use it to copy the plugin Javascript files.
+                    return ResourcesLoader.xhrGet("app-bundle:///cordova_plugins.json");
+                })
+                .then(function(xhr) {
+                    return JSON.parse(xhr.responseText.trim());
+                })
+                .then(function(plugins){
+                    if (!plugins) {
+                        throw new Error('Error loading cordova_plugins.json');
+                    }
+
+                    var copies = [
+                        copyFile("app-bundle:///cordova.js", cordovaFile),
+                        copyFile("app-bundle:///crx_files/config." + platformId + ".xml", platformDirectory + "config.xml"),
+                        copyFile("app-bundle:///cordova_plugins.json", platformWWWDirectory + "cordova_plugins.json"),
+                        copyFile("app-bundle:///chromeapp.html", platformWWWDirectory + "chromeapp.html"),
+                        copyFile("app-bundle:///chromeappstyles.css", platformWWWDirectory + "chromeappstyles.css"),
+                        copyFile("app-bundle:///chromebgpage.html", platformWWWDirectory + "chromebgpage.html")
+                    ];
+
+                    for(var i = 0; i < plugins.length; i++) {
+                        console.log('copying ' + plugins[i].file);
+                        copies.push(copyFile("app-bundle:///" + plugins[i].file, platformWWWDirectory + plugins[i].file));
+                    }
+
+                    return Q.all(copies);
+                })
+                .then(function(){
+                    return ResourcesLoader.appendFileContents(cordovaFile, dataToAppend);
+                });
+            }
+        });
+    }]);
+})();
